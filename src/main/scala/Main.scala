9185d0ce47012648d06754fd2471321bee7e1d9f
AminMalekloo <amin.malekloo@yahoo.com>
2022-08-13 20:16:45 +0430

    feat (Main.scala): moved application logic to process layer and implemented cake pattern in Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,42 @@
+import models.{Arguments, Interval}
+import process.OrderProcess
+import repo.impl.OrderItemsRepoImpl
+
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.Await
+import scala.util.{Failure, Success}
+
 object Main {
+
+  lazy val defaultIntervals = Seq(
+    Interval("1-3", 1),
+    Interval("4-6", 2),
+    Interval("7-12", 3),
+    Interval(">12", 4),
+  )
+
+  def main(args: Array[String]): Unit = {
+    import bootstrap.ec
+    val taskTimeoutValue = bootstrap.config.getInt("taskTimeoutValue")
+    val taskTimeoutUnit = TimeUnit.valueOf(bootstrap.config.getString("taskTimeoutUnit"))
+
+    val orderProcess = new OrderProcess(new OrderItemsRepoImpl)
+
+    Parser.parseArguments(args)(defaultIntervals) match {
+      case Failure(exception) =>
+        exception.printStackTrace()
+        sys.exit(1)
+      case Success(Arguments(start, end, intervals)) =>
+        val result = Await.result(
+          orderProcess.getOrdersCountGroupedByProductAgeInInterval(start, end, intervals),
+          FiniteDuration(taskTimeoutValue, taskTimeoutUnit)
+        )
+        result.foreach {
+          case (intervalTitle, count) =>
+            println(s"$intervalTitle: $count orders")
+        }
+    }
+  }
 
 }
